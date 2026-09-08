fde092724da62da92145d377040b444b7010567a
Hassan Rahhal <hassanrahhal09@gmail.com>
2026-09-08 11:50:44 +0200

    Adding spaces

--- src/Home.jsx
+++ src/Home.jsx
@@ -145,7 +145,7 @@ function Home() {
           </div>
         </div>
         <div className="footer-bottom">
-          <Link to="/portal-x7k9-login" className="admin-entry-link">© 2026 HIJAB HOME
+          <Link to="/portal-x7k9-login" className="admin-entry-link">© 2026 HIJAB HOME 
           <span> DESIGNED WITH CARE</span></Link>
           
         </div>
